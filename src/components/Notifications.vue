5d3eefa2c01d27d175dd268134f92a2df9ed9511
Richard Bondi <r_bondi@hotmail.com>
2020-09-28 11:45:48 -0500

    accept request

--- src/components/Notifications.vue
+++ src/components/Notifications.vue
@@ -4,13 +4,15 @@
 
     <ul>
       <li v-for="n in notifications" :key="n.id">
-        <div class="notification">
+        <div class="notification" @click="handleNotificationClick(n)">
           <div class="notification-img-container" v-if="n.avatar_image">
             <img class="notification-avatar-img" :src="`${IMAGE_BASE_URL}/${n.avatar_image}`" />
           </div>
           {{n.fullname}}&nbsp;
-          <!-- <em>{{n.username}}</em> -->
           {{notificationText(n)}}
+          <button class="request-button" v-if="enableAcceptButton(n)" @click="handleAcceptRequest(n)">
+            Accept request
+          </button>
           <div>{{showTime(n.created_at)}}</div>
           <div v-if="n.notification_type!='request'">
             <div class="notification-content">{{n.content}}</div>
@@ -23,8 +25,8 @@
 
 <script>
 import { showTime } from "../util";
-import { notifications } from "../api";
-import { IMAGE_BASE_URL } from "../constants";
+import { notifications, updaterequest } from "../api";
+import { IMAGE_BASE_URL, REQUEST } from "../constants";
 export default {
   name: "Notifications",
   data() {
@@ -40,14 +42,33 @@ export default {
         case "message":
           return "sent you a new message";
         case "request":
+          const content = this.requestContent(n)
           return (
             "requested " +
-            (n.content === "accountability"
+            (content.name === "accountability"
               ? "an accountability partnership"
               : "a mentorship")
           );
       }
     },
+    requestContent(n) {
+      const csv = n.content.split(',')
+      return {name: csv[0], status: parseInt(csv[1], 10)}
+    },
+    enableAcceptButton(n) {
+      return n.notification_type==='request' && this.requestContent(n).status === REQUEST.STATUS.PENDING
+    },
+    handleNotificationClick(n) {
+      console.log(n)
+    },
+    handleAcceptRequest(n) {
+      updaterequest(n.source_id, REQUEST.STATUS.ACTIVE).then(response => {
+        console.log(response)
+        if (response.success) {
+          n.content = this.requestContent(n).name+',1'
+        }
+      }).catch(console.log)
+    }
   },
   mounted() {
     notifications().then((data) => {
@@ -86,5 +107,10 @@ export default {
 
 .notification-content {
   color: #9999aa;
+}
+
+.request-button {
+  float: right;
+  max-width: 100px;
 }
 </style>
