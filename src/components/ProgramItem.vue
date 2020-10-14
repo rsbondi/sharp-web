28087c5fe7fdf0d7572b28804c4e4f5a13b868a5
Richard Bondi <r_bondi@hotmail.com>
2020-10-14 18:36:00 -0500

    show user program status

--- src/components/ProgramItem.vue
+++ src/components/ProgramItem.vue
@@ -4,12 +4,14 @@
       <div class="flex-container">
         <div class="info">{{program.name}}</div>
         <div class="info">Level: {{program.level}}</div>
+        <div class="info">{{mine}}</div>
       </div>
       <div class="desc">{{program.description}}</div>
     <div v-for="phase in program.phases" :key=phase.id>
       <div class="flex-container">
         <div class="info">{{phase.name}}</div>
         <div class="info">Level: {{phase.level}}</div>
+        <div class="info">&nbsp;</div>
       </div>
       <div class="desc">{{phase.description}}</div>
     </div>
@@ -21,6 +23,13 @@
 export default {
   props: {
     program: Object
+  },
+  computed: {
+    mine() {
+      if (this.program.creator) return 'creator'
+      if (this.program.member) return 'member'
+      return ''
+    }
   }
 }
 </script>
@@ -30,12 +39,13 @@ export default {
   margin-top: 2em;
   border: 1px solid lightgray;
   border-radius: 5px;
+  box-shadow: 0.5em 0.5em 0.5em rgba(0,0,0,0.2)
 }
 .desc {
   padding: 1em;
 }
 .info {
-  width: 50%;
+  width: 33%;
   padding: 1em;
 }
 .info:first-child {
