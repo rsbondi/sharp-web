523139173c6fd02c42b2d6586f05cc979b8da965
Richard Bondi <r_bondi@hotmail.com>
2020-10-15 17:34:10 -0500

    half star ratings

--- src/components/Rating.vue
+++ src/components/Rating.vue
@@ -5,35 +5,40 @@
       :class="rating > 0 ? 'full' : 'empty'"
       @click="rateit(1)"
       >
-      <Star/>
+      <Star v-if="rating ===0 || rating > 0.5"/>
+      <StarHalf v-if="rating > 0 && rating <= 0.5" />
     </div>
     <div 
       class="star" 
       :class="rating > 1 ? 'full' : 'empty'"
       @click="rateit(2)"
       >
-      <Star/>
+      <Star v-if="rating <=1 || rating > 1.5"/>
+      <StarHalf v-if="rating > 1 && rating <= 1.5" />
     </div>
     <div 
       class="star" 
       :class="rating > 2 ? 'full' : 'empty'"
       @click="rateit(3)"
       >
-      <Star/>
+      <Star v-if="rating <=2 || rating > 2.5"/>
+      <StarHalf v-if="rating > 2 && rating <= 2.5" />
     </div>
     <div 
       class="star" 
       :class="rating > 3 ? 'full' : 'empty'"
       @click="rateit(4)"
       >
-      <Star/>
+      <Star v-if="rating <=3 || rating > 3.5"/>
+      <StarHalf v-if="rating > 3 && rating <= 3.5" />
     </div>
     <div 
       class="star" 
       :class="rating > 4 ? 'full' : 'empty'"
       @click="rateit(5)"
       >
-      <Star/>
+      <Star v-if="rating <=4 || rating > 4.5"/>
+      <StarHalf v-if="rating > 4 && rating <= 4.5" />
     </div>
     <div class="star ratings">({{nratings}})</div>
   </div>
@@ -43,7 +48,7 @@
 import Star from './icons/Star.vue'
 import StarHalf from './icons/StarHalf.vue'
 export default {
-  components: { Star },
+  components: { Star, StarHalf },
   props: {
     rating: Number,
     nratings: Number,
@@ -66,7 +71,7 @@ export default {
     fill: none;
   }
   .full {
-    fill: gray;
+    fill: gold;
   }
   .ratings {
     padding-top: 0.2em;
