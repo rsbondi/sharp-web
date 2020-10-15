7f6b105800ba570a90a875f4ba3ec8f3a909cdd6
Richard Bondi <r_bondi@hotmail.com>
2020-10-15 14:00:27 -0500

    user info to program w initial dummy rating

--- src/components/ProgramItem.vue
+++ src/components/ProgramItem.vue
@@ -2,51 +2,88 @@
   <div class="program-item">
     <div class="phase">
       <div class="flex-container">
-        <div class="info">{{program.name}}</div>
-        <div class="info">Level: {{program.level}}</div>
-        <div class="info">Members: {{program.members}}</div>
+        <div class="info">{{ program.name }}</div>
+        <div class="info">Level: {{ program.level }}</div>
+        <div class="info">Members: {{ program.members }}</div>
         <div class="info mystatus">
-          {{mine}}
+          {{ mine }}
           <button @click="joinProgram" v-if="!mine">Join</button>
         </div>
       </div>
-      <div class="desc">{{program.description}}</div>
-    <div v-for="phase in program.phases" :key=phase.id>
-      <div class="flex-container">
-        <div class="info">{{phase.name}}</div>
-        <div class="info">Level: {{phase.level}}</div>
-        <div class="info">&nbsp;</div>
-        <div class="info">&nbsp;</div>
+      <div class="desc">{{ program.description }}</div>
+      <div v-for="phase in program.phases" :key="phase.id">
+        <div class="flex-container">
+          <div class="info">{{ phase.name }}</div>
+          <div class="info">Level: {{ phase.level }}</div>
+          <div class="info">&nbsp;</div>
+          <div class="info">&nbsp;</div>
+        </div>
+        <div class="desc">{{ phase.description }}</div>
       </div>
-      <div class="desc">{{phase.description}}</div>
-    </div>
+      <div class="flex-container">
+        <div class="about">
+          <strong>Created by</strong><br>
+          <div class="prg-img-container" v-if="program.avatar_image">
+            <img
+              class="prg-avatar-img"
+              :src="`${IMAGE_BASE_URL}/${program.avatar_image}`"
+            />
+          </div>
+          <div class="userinfo">
+            {{ program.fullname }} 
+            <ProfileLink :user="program" />
+            <button @click="messageContact" class="action-primary">Message</button>
+
+          </div>
+          <div style="clear:both"></div>
+        </div>
+        <div class="about stars">
+          <strong>Rating</strong><br>
+          <div>
+            <Rating />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { participate } from '../api'
+import { participate } from "../api";
+import ProfileLink from "./ProfileLink.vue";
+import Rating from './Rating.vue'
+import { IMAGE_BASE_URL } from "../constants";
+
 export default {
   props: {
-    program: Object
+    program: Object,
   },
+  components: { ProfileLink, Rating },
   methods: {
     joinProgram() {
-      participate(this.program.id).then(result => {
-        console.log(result)
-        this.program.member = 1
-        this.program.members++
-      })
+      participate(this.program.id).then((result) => {
+        console.log(result);
+        this.program.member = 1;
+        this.program.members++;
+      });
+    },
+    messageContact() {
+      this.$store.dispatch('messageuser', {username: this.program.username, user_id: this.program.user_id})
     }
+  },
+  data() {
+    return {
+      IMAGE_BASE_URL: IMAGE_BASE_URL,
+    };
   },
   computed: {
     mine() {
-      if (this.program.creator) return 'you created'
-      if (this.program.member) return 'you joined'
-      return ''
-    }
-  }
-}
+      if (this.program.creator) return "you created";
+      if (this.program.member) return "you joined";
+      return "";
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -54,7 +91,7 @@ export default {
   margin-top: 2em;
   border: 1px solid lightgray;
   border-radius: 5px;
-  box-shadow: 0.5em 0.5em 0.5em rgba(0,0,0,0.2)
+  box-shadow: 0.5em 0.5em 0.5em rgba(0, 0, 0, 0.2);
 }
 .desc {
   padding: 1em;
@@ -68,6 +105,29 @@ export default {
 }
 .mystatus {
   font-style: italic;
-  color: lightslategray
+  color: lightslategray;
+}
+
+.prg-avatar-img {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+}
+
+.prg-img-container {
+  float: left;
+  margin-right: 1em;
+  margin-top: 0.5em;
+}
+
+.about {
+  width: 50%;
+  padding: 1em;
+}
+.userinfo {
+  padding-top: 18px;
+}
+.stars {
+  padding-top: 14px;
 }
 </style>
---
+++ src/components/Rating.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex-container">
+    <div 
+      class="star" 
+      :class="rating > 0 ? 'full' : 'empty'"
+      @click="rateit(1)"
+      >
+      <Star/>
+    </div>
+    <div 
+      class="star" 
+      :class="rating > 1 ? 'full' : 'empty'"
+      @click="rateit(2)"
+      >
+      <Star/>
+    </div>
+    <div 
+      class="star" 
+      :class="rating > 2 ? 'full' : 'empty'"
+      @click="rateit(3)"
+      >
+      <Star/>
+    </div>
+    <div 
+      class="star" 
+      :class="rating > 3 ? 'full' : 'empty'"
+      @click="rateit(4)"
+      >
+      <Star/>
+    </div>
+    <div 
+      class="star" 
+      :class="rating > 4 ? 'full' : 'empty'"
+      @click="rateit(5)"
+      >
+      <Star/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Star from './icons/Star.vue'
+import StarHalf from './icons/StarHalf.vue'
+export default {
+  components: { Star },
+  data() {
+    return {
+      rating: 0
+    }
+  },
+  methods: {
+    rateit(n) {
+      console.log('rateit', n)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .star {
+    float: left;
+    cursor: pointer;
+  }
+  .empty {
+    fill: none;
+  }
+  .full {
+    fill: gray;
+  }
+</style>
