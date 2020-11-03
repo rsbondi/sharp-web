a547786c7e524cda0beba9b6fb1ce0fe251fd9ca
Richard Bondi <r_bondi@hotmail.com>
2020-11-03 14:16:05 -0600

    bio display

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="fullname">{{user.fullname}}</div>
+  <div v-if="user.bio" class="flex-container">
+    <div class="bio">{{user.bio}}</div>
+  </div>
   <div class="flex-container">
     <div class="follow"><a href="" @click.prevent="following">Following {{user.following}}</a></div>
     <div class="follow"><a href="" @click.prevent="followers">Followers {{user.followers}}</a></div>
@@ -37,5 +40,10 @@ export default {
 
   .fullname {
     font-size: 1.15em;
+  }
+
+  .bio {
+    margin-top: 1em;
+    text-align: left;
   }
 </style>
