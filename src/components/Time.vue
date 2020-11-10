c7bba80126071140e28f52c145d9fe1d7e6b09da
Richard Bondi <r_bondi@hotmail.com>
2020-11-10 12:16:35 -0600

    time display glitch

--- src/components/Time.vue
+++ src/components/Time.vue
@@ -12,7 +12,10 @@ export default {
       const created = Date.parse(this.created_at.replace(" ", "T") + "Z")
       const now = this.$store.state.timer
       const diffSecs = Math.floor((now - created) / 1000)
-      if (diffSecs < 60) {
+      if (diffSecs < 10) {
+        return `now`
+      }
+      else if (diffSecs < 60) {
         return `${diffSecs} ${this.plural("second", diffSecs)}`
       } else if (diffSecs < 60 * 60) {
         const m = Math.floor(diffSecs / 60)
