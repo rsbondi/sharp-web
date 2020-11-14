07f44ae4ae109337ffebb70f5a572d6a9dc22f53
Richard Bondi <r_bondi@hotmail.com>
2020-11-14 15:02:09 -0600

    program help tab

--- src/components/Program.vue
+++ src/components/Program.vue
@@ -3,9 +3,11 @@
     <ul>
       <li @click="setMode('browse')" :class="mode==='browse' ? 'selected' : ''">Browse</li>
       <li @click="setMode('create')" :class="mode==='create' ? 'selected' : ''">Create</li>
+      <li @click="setMode('help')" :class="mode==='help' ? 'selected' : ''" title="info">🛈</li>
     </ul>
     <ProgramCreate v-if="mode==='create'"/>
     <Programs v-if="mode==='browse'"/>
+    <ProgramHelp v-if="mode==='help'"/>
     <ProgramFeed v-if="mode==='feed'"/>
   </div>
 </template>
@@ -14,8 +16,9 @@
 import ProgramCreate from './ProgramCreate.vue'
 import Programs from './Programs.vue'
 import ProgramFeed from './ProgramFeed.vue'
+import ProgramHelp from './ProgramHelp.vue'
 export default {
-  components: { ProgramCreate, Programs, ProgramFeed },
+  components: { ProgramCreate, Programs, ProgramFeed, ProgramHelp },
   computed: {
     mode() {
       return this.$store.state.programView
---
+++ src/components/ProgramHelp.vue
@@ -0,0 +1,61 @@
+<template>
+  <h2>Programs</h2>
+
+  <h3>Objective</h3>
+  
+  <p>
+    The idea behind the program section is for users to create programs that find a balance between what
+    nature reuqires and what society demands.  They should also be created with trying to find a balance
+    between desired level of health and sustainability.  Too strict of a program will be 
+    difficult for most.  Too loose, won't be healthy.
+  </p>
+
+  <p>
+    Programs created here are available for all users to join.  Each one has its discussion feed
+    where you can share your ideas on the program with others.  You can rate any program that you 
+    join.
+  </p>
+
+  <h3>Nature</h3>
+  
+  <p>
+    Nature operates by immutable, predefined rules.  They are on one hand very complex, but on another
+    very simple.  They are well understood and have been for quite some time.  The simple side
+    is that we are designed by nature to eat raw fruits, vegetables, nuts and seed, keeping fat to
+    a minimum, and avoiding processed foods, especially SOS(salt, oils and sugar).  
+    I know, it
+    is not what you want to here, but nature does not care.  As mentioned above, it will be difficult
+    for most.  For some, it may be difficult to believe, but again, nature's rules are immutable and
+    will not shape to our beliefs.
+  </p>
+
+  <h3>Society</h3>
+  
+  <p>
+    We have been taught various customs that are centered around food.  Weddings, funerals, birthdays,
+    rites of passage, etc. all are celebrated with a feast.  Growing up, most of us have eaten
+    predominately cooked foods.  We have been rewarded with foods, food has been our consolation
+    in hard times, we have these amoung other emotional attachments. I hope this sheds some light
+    on the conflict between nature and society.
+  </p>
+
+  <h3>Programs</h3>
+
+  <p>
+    Programs created here are available for all users to join.  Each one has its discussion feed
+    where you can share your ideas on the program with others.  You can rate any program that you 
+    join.
+  </p>
+  <h4>Program levels</h4>
+  <p>
+    The "ideal" is as mentioned, raw fruits, vegetables, nuts and seeds.  Setting a level for a program
+    is an indication of how close to this "ideal" the program is.  This should give users wishing to
+    join an idea of the difficulty in sustaining the program.  Levels range from 0-100, where 100
+    is the ideal, and 0, what's the point.
+  </p>
+
+  <h4>Program Phases</h4>
+  You can optionally create the program in phases, for example if you want to work towards and 80%
+  program, but want to gradually get there you can start with say 60% and work your way to where the
+  final phase would be the 80%
+</template>
